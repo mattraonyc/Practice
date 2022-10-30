34f94aec2f187da4a1b175fb1c3b5b689bcb2b0f
mattraonyc <mattraonyc@gmail.com>
2022-10-30 12:21:49 -0700

    Practice files

---
+++ Oldagram/index.js
@@ -0,0 +1,44 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+const nameOne = document.getElementById("name-one")
+const nameTwo = document.getElementById("name-two")
+const nameThree = document.getElementById("name-three")
+const likeButton = document.getElementById("like-button")
+
+function render (info) {
+nameOne.innerHTML = posts[0].name;
+nameTwo.innerHTML = posts[1].name;
+nameThree.innerHTML = posts[2].name;
+
+}
+
+render()
+
